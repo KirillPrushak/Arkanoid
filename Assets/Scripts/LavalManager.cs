18605f9adcc7518a7926519e812a5867169d4987
KirillPrushak <kirillprushak@gmail.com>
2023-04-10 23:23:41 +0300

    Add menu

---
+++ Assets/Scripts/LavalManager.cs
@@ -0,0 +1,15 @@
+﻿using DefaultNamespace.Ui;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LavalManager : MonoBehaviour
+    {
+        [SerializeField] private LevelsConfigs _levelsConfigs;
+
+        public void SetCurrentLevelCompleted()
+        {
+            
+        }
+    }
+}
